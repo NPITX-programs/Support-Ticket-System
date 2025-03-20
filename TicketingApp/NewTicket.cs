c8de2ec8d9c5705cbdead17ab8f3f2ed8516959e
Charlie Hatch <chatch@npitx.org>
2025-03-20 14:46:31 -0500

    extract zip

---
+++ TicketingApp/NewTicket.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketingApp
+{
+    internal class NewTicket
+    {
+        internal string Name;
+        internal string AssignedPerson;
+        internal string Status;
+        internal string Priority;
+        internal string Problem;
+        internal string Workstation;
+        
+        
+
+    }
+
+}
